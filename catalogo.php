7534b6fb64511f9e89c80b56ae500b96118bb779
Christan RCSG <christian.floppy@gmail.com>
2019-06-25 20:46:12 -0500

    Catalogo Inicio
    Se estra trabajando en el catalogo

---
+++ catalogo.php
@@ -0,0 +1,405 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Froger - Hojalateria y Pintura</title>
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="stylesheet" href="style/style.css">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="format-detection" content="telephone=no">
+    <meta name="title" content="FRODGER Taller de reparación de automóviles en Ciudad de México" />
+    <meta name="description" content="Empresa que brinda servicio de hojalateria y pintura con mano de obra especializada y precios razonables buscando garantizar y superar las expectativas de nuestros clientes en la reparación de su automóvil." />
+    <meta name="autor" content="ReCS!Gua-StuDios"/>
+    <link rel="alternate" href="http://www.frodger.com/" hreflang="es-MX">
+    <script src="js/scripts.js"></script>
+</head>
+<body  id="frodger" data-spy="scroll" data-target=".navbar" data-offset="60">
+	<!--INICIANDO LA ESTRUCTURA BASICA ENCABEZADO-->
+	<header id="" class="colorSemiDegradado header1">
+		<div class="contH colorSemiDegradado">
+			<div class="contLogo">
+				<a href="index.html">
+				<img class="logoImg" src="images/frodger_logo.png" alt="Logo de FRDOGER"></a>
+			</div>
+			<div class="contName">
+				<h1 class="h1Header">Hojalatería <img class="circleImg" src="images/color.png" alt="CiruloFrodger"> pintura </h1>
+				<div class="ajedres">
+				</div>
+			</div>
+			<div class="contName contLogosHeader">
+				<div class="credes">
+					<a target="_blank" href="https://www.facebook.com/Frodger.CDMX/">
+						<img class="logoRedes" src="images/facebook.png" alt="Logo de Facebook"></a>
+					<a href="https://www.instagram.com/frodger.oficial/?hl=es-la"  target="_blank">
+						<img class="logoRedes" src="images/instagram.png" alt="Logo de Facebook"></a>
+					<img class="logoRedes" src="images/whatsapp.png" alt="Logo de Facebook">
+					<div class="txtTel">
+						<a  href="https://wa.me/525686635038" target="_blank">
+							<h1>56 8663 5038</h1>	
+						</a>
+					</div>
+				</div>
+				<div class="ajedres">
+					<div class="cuadrito">					
+						<div class="c b c10"></div>
+						<div class="c "></div>						
+						<div class="c b c10"></div>
+						<div class="c "></div>						
+						<div class="c b c9"></div>
+						<div class="c "></div>						
+						<div class="c b c9"></div>
+						<div class="c "></div>						
+						<div class="c b c8"></div>
+						<div class="c "></div>
+						<div class="c b c8"></div>
+						<div class="c "></div>
+						<div class="c b c7"></div>
+						<div class="c "></div>
+						<div class="c b c7"></div>
+						<div class="c "></div>
+						<div class="c b c6"></div>
+						<div class="c "></div>
+						<div class="c b c6"></div>
+						<div class="c "></div>						
+						<div class="c b c5"></div>
+						<div class="c "></div>						
+						<div class="c b c5"></div>
+						<div class="c "></div>						
+						<div class="c b c4"></div>
+						<div class="c "></div>						
+						<div class="c b c4"></div>
+						<div class="c "></div>						
+						<div class="c b c3"></div>
+						<div class="c "></div>
+						<div class="c b c3"></div>
+						<div class="c "></div>
+						<div class="c b c2"></div>
+						<div class="c "></div>
+						<div class="c b c2"></div>
+						<div class="c "></div>
+						<div class="c b c1"></div>
+						<div class="c "></div>
+						<div class="c b c1"></div>
+						<div class="c "></div>
+					</div>
+					<div class="cuadrito">
+						<div class="c "></div>						
+						<div class="c b c10"></div>
+						<div class="c "></div>						
+						<div class="c b c10"></div>
+						<div class="c "></div>						
+						<div class="c b c9"></div>
+						<div class="c "></div>						
+						<div class="c b c9"></div>
+						<div class="c "></div>						
+						<div class="c b c8"></div>
+						<div class="c "></div>
+						<div class="c b c8"></div>
+						<div class="c "></div>
+						<div class="c b c7"></div>
+						<div class="c "></div>
+						<div class="c b c7"></div>
+						<div class="c "></div>
+						<div class="c b c6"></div>
+						<div class="c "></div>
+						<div class="c b c6"></div>
+						<div class="c "></div>						
+						<div class="c b c5"></div>
+						<div class="c "></div>						
+						<div class="c b c5"></div>
+						<div class="c "></div>						
+						<div class="c b c4"></div>
+						<div class="c "></div>						
+						<div class="c b c4"></div>
+						<div class="c "></div>						
+						<div class="c b c3"></div>
+						<div class="c "></div>
+						<div class="c b c3"></div>
+						<div class="c "></div>
+						<div class="c b c2"></div>
+						<div class="c "></div>
+						<div class="c b c2"></div>
+						<div class="c "></div>
+						<div class="c b c1"></div>
+						<div class="c "></div>
+						<div class="c b c1"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="contAjedrezBig colorSemiDegradado">
+			<div class="mitadHome">
+				<div class="BigAge">
+					<div class="bCuadro">					
+						<div class="C b C1"></div>
+						<div class="C "></div>						
+						<div class="C b C2"></div>
+						<div class="C "></div>
+						<div class="C b C3"></div>
+						<div class="C "></div>	
+						<div class="C b C4"></div>
+						<div class="C "></div>
+						<div class="C b C5"></div>
+						<div class="C "></div>				
+					</div>
+					<div class="bCuadro">
+						<div class="C "></div>						
+						<div class="C b C2"></div>
+						<div class="C "></div>						
+						<div class="C b C3"></div>
+						<div class="C "></div>						
+						<div class="C b C4"></div>
+						<div class="C "></div>						
+						<div class="C b C5"></div>
+					</div>
+					<div class="bCuadro">					
+						<div class="C b C3"></div>
+						<div class="C "></div>						
+						<div class="C b C4"></div>
+						<div class="C "></div>	
+						<div class="C b C5"></div>
+						<div class="C "></div>	
+						<div class="C b C6"></div>			
+					</div>
+					<div class="bCuadro">
+						<div class="C "></div>						
+						<div class="C b C4"></div>
+						<div class="C "></div>						
+						<div class="C b C5"></div>
+						<div class="C "></div>						
+						<div class="C b C6"></div>
+						<div class="C "></div>
+					</div>		
+					<div class="bCuadro">					
+						<div class="C b C5"></div>
+						<div class="C "></div>						
+						<div class="C b C6"></div>
+						<div class="C "></div>	
+						<div class="C b C7"></div>
+						<div class="C "></div>				
+					</div>
+					<div class="bCuadro">
+						<div class="C "></div>						
+						<div class="C b C7"></div>
+						<div class="C "></div>						
+						<div class="C b C8"></div>
+						<div class="C "></div>						
+					</div>	
+					<div class="bCuadro">					
+						<div class="C b C8"></div>
+						<div class="C "></div>						
+						<div class="C b C9"></div>
+						<div class="C "></div>				
+					</div>
+					<div class="bCuadro">
+						<div class="C "></div>						
+						<div class="C b C9"></div>
+						<div class="C "></div>
+					</div>	
+					<div class="bCuadro">					
+						<div class="C b C9"></div>
+						<div class="C "></div>			
+					</div>
+				</div>
+			</div>
+			<div class="mitadHome2">
+			</div>
+		</div>
+	</header>
+	
+	<!--ARTS CONTENEDORES DE SECCIONES-->
+	<section>
+		<div class="slider">
+			<div class="boxAbout">
+				<div class="flechaSlider">
+					<img class="arrowSLideriImg" src="images/left-arrow.png" alt="preview Icon">
+				</div>
+				<div class="imgSlider">
+					<img class="imgPromo" src="images/promo.jpeg" alt="">
+				</div>
+				<div class="imgSlider">
+					<h1 class="txtSLiderH1">¡Tu nave como nueva al mejor precio!</h1>
+					<p class="txtSLider">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor commodi sunt numquam nihil veniam at voluptates possimus officiis fugiat, quam praesentium iure.
+					</p>
+				</div>
+				<div class="flechaSlider">
+					<img class="arrowSLideriImg" src="images/right-arrow.png" alt="next icon">
+				</div>
+			</div>
+		</div>
+	</section>
+	<section>
+		<div id="contacto" class="contact">
+			<div class="titulo2">
+				<div class="SubtitleName">
+					<div class="tituloPrincipalContServ">
+						<h2 class="ttsH1"><img class="circleImgTittle" src="images/color.png" alt="CiruloFrodger"> CONTACTO</h2>					
+					</div>
+				</div>
+				<div class="ContAjedresTittle">
+					<div class="BigAge">
+						<div class="bCuadro">					
+							<div class="CT b c10"></div>
+							<div class="CT  "></div>
+							<div class="CT b c9"></div>
+							<div class="CT "></div>						
+							<div class="CT b c7"></div>
+							<div class="CT "></div>
+							<div class="CT b c5"></div>
+							<div class="CT "></div>	
+							<div class="CT b c4"></div>
+							<div class="CT "></div>
+							<div class="CT b c3"></div>
+							<div class="CT "></div>	
+							<div class="CT b c2"></div>
+							<div class="CT "></div>	
+							<div class="CT b c1"></div>
+							<div class="CT "></div>					
+						</div>
+						<div class="bCuadro">
+							<div class="CT"></div>
+							<div class="CT b c10"></div>
+							<div class="CT "></div>		
+							<div class="CT b c9"></div>
+							<div class="CT "></div>						
+							<div class="CT b c7"></div>
+							<div class="CT "></div>						
+							<div class="CT b c5"></div>
+							<div class="CT "></div>						
+							<div class="CT b c4"></div>
+							<div class="CT "></div>						
+							<div class="CT b c3"></div>
+							<div class="CT "></div>						
+							<div class="CT b c2"></div>
+							<div class="CT "></div>						
+							<div class="CT b c1"></div>
+						</div>
+
+						<div class="bCuadro">	
+							<div class="CT b c10"></div>
+							<div class="CT "></div>				
+							<div class="CT b c9"></div>
+							<div class="CT "></div>						
+							<div class="CT b c7"></div>
+							<div class="CT "></div>
+							<div class="CT b c5"></div>
+							<div class="CT "></div>	
+							<div class="CT b c4"></div>
+							<div class="CT "></div>
+							<div class="CT b c3"></div>
+							<div class="CT "></div>	
+							<div class="CT b c2"></div>
+							<div class="CT "></div>	
+							<div class="CT b c1"></div>
+							<div class="CT "></div>							
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="contAddress contTxtBox">
+				<div class="addresMi">
+					<div class="txtDataContacto">
+						<img class="imgRedes" src="images/marcador-de-localizacion.png">
+						<p class="txtData">
+							Av. Rio Consulado 1546, Vallejo, 07870 Ciudad de México, CDMX
+						</p>
+					</div>
+					<div class="txtDataContacto">
+						<img class="imgRedes" src="images/reloj-de-pared.png">
+						<p class="txtData">
+							Lunes a Viernes 09:00 hrs. - 17:00 hrs.
+						</p>
+					</div>
+				</div>
+				<div class="addresMi">
+					<div class="txtDataContacto">
+						<img class="imgRedes" src="images/telephone.png">
+						<p class="txtData">
+							Av. Rio Consulado 1546, Vallejo, 07870 Ciudad de México, CDMX
+						</p>
+					</div>
+					<div class="txtDataContacto">
+						<img class="imgRedes" src="images/mail.png">
+						<p class="txtData">
+							Lunes a Viernes 09:00 hrs. - 17:00 hrs.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="contMap">
+				<iframe src="https://www.google.com/maps/d/embed?mid=12rC5u9GMB8h6AVutffW3SvtaYHM_Nvhp" width="100%" height="100%"></iframe>
+				<div class="contForm colorSemiDegradado">
+					<div class="solidContForm container">
+						<form action="send.php">
+							<h2 class="txtCotiza3">Cotiza envianodos tu información:</h2>
+						    <label for="fname">*Nombre</label>
+						    <input type="text" id="fname" name="firstname" placeholder="Escriba su nombre..." required>
+						    <label for="ftel">*Telefono</label>
+						    <input type="tel" id="ftel" name="telefono" placeholder="Escriba su teléfono..." required>
+						    <label for="fmail">*Correo Electronico</label>
+						    <input type="email" id="fmail" name="mail" placeholder="Escriba su correo electrónico..." required>
+						    <label for="fmarca">*Marca</label>
+						    <input type="tel" id="fmarca" name="marca" placeholder="Ejemplo (Nissan, Ford, Toyota)" required>
+						    <label for="fmodelo">*Modelo</label>
+						    <input type="text" id="fmodelo" name="modelo" placeholder="Ejemplo (Aveo, Sentra, Chevy)" required>
+						    <label for="fanio">Año</label>
+						    <select id="fanio" name="anio">
+						      <option value="2019">2019</option>
+						      <option value="2018">2018</option>
+						      <option value="2017">2017</option>
+						      <option value="2016">2016</option>
+						      <option value="2015">2015</option>
+						      <option value="2014">2014</option>
+						      <option value="2013">2013</option>
+						      <option value="2012">2012</option>
+						      <option value="2011">2011</option>
+						      <option value="2010">2010</option>
+						      <option value="2009">2009</option>
+						      <option value="2008">2008</option>
+						      <option value="2007">2007</option>
+						      <option value="2006">2006</option>
+						      <option value="2004">2004</option>
+						      <option value="2003">2003</option>
+						      <option value="2002">2002</option>
+						      <option value="2001">2001</option>
+						      <option value="2000">2000</option>
+						      <option value="1999">1999</option>
+						      <option value="1998">1998</option>
+						      <option value="1997">1997</option>
+						      <option value="1996">1996</option>
+						      <option value="1995">1995</option>
+						      <option value="1994">1994</option>
+						      <option value="1993">1993</option>
+						      <option value="1992">1992</option>
+						      <option value="1991">1991</option>
+						      <option value="1990">1990</option>
+						      <option value="1900">Anterior</option>
+						    </select>
+		    				<label for="subject">Comentario</label>
+    						<textarea id="subject" name="asunto" placeholder="Esciba su duda o comentario." required></textarea>
+						    <input type="submit" value="Enviar">
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<!--Pie de pagina-->
+	<footer>
+		<div class="contf1">
+		</div>
+		<div class="contf2">
+			<img class="imgLogoFR" src="images/iconPageBR.png" alt="Logo Frodger FR">
+			<h1 class="txtCotiza3">Aviso de Privacidad </h1>
+			<h1 class="txtCotiza3">Terminos y Condiciones </h1>
+			<h1 class="txtCotiza3">© 2019 FRODGER S.A. de C.V. Derechos Reservados. </h1>
+		</div>
+		<div class="contf3">
+			<img class="imgLogoFR imgESR" src="images/esr.png" alt="Empresa Socialmente Resposable">
+		</div>
+	</footer>
+</body>
+</html>
